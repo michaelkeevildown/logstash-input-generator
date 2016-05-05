202b886a41a39da919096cb3c26fccac17415f71
Michael Down <down@elastic.co>
2016-05-05 10:14:03 +0100

    changed schema default interval to 10

--- lib/logstash/inputs/generator.rb
+++ lib/logstash/inputs/generator.rb
@@ -26,7 +26,7 @@ class LogStash::Inputs::Generator < LogStash::Inputs::Base
 
   # When using a schema file, this setting will indicate how frequently
   # (in seconds) logstash will check the schema file for updates.
-  config :schema_refresh_interval, :validate => :number, :default => 1, :required => true
+  config :schema_refresh_interval, :validate => :number, :default => 10, :required => true
 
   ## Logstash Config Params ##
 
